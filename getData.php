d7f3a1c3d55e7f99c4b3c9cb465ddc2b19e4ef90
Matt Giguere <matthew.giguere@gmail.com>
2014-07-26 20:47:04 -0400

    commented out echoed info.

--- getData.php
+++ getData.php
@@ -1,8 +1,38 @@
 <?php
-$afspath = $_SERVER["AeroFSdir"];
-$credsfile = $afspath . '.credentials/SQL/csaye';
-$file = file_get_contents($credsfile);
-echo "The host is: ";
-echo $file[0];
-echo $file;
+    $afspath = $_SERVER["AeroFSdir"];
+    $credsfile = $afspath . '.credentials/SQL/csaye';
+    $file = file_get_contents($credsfile);
+    //echo "The host is: ";
+    //echo $file[0];
+    //echo $file;
+
+    $server = mysql_connect($host, $username, $password);
+    $connection = mysql_select_db($database, $server);
+
+    $myquery = "
+    SELECT v.jd, v.mnvel FROM velocities v INNER JOIN observations o ON  o.observation_id=v.observation_id WHERE o.object='10700';
+    ";
+    /*
+    $myquery = "
+    SELECT  `date`, `close` FROM  `data2`
+    ";
+    */
+
+    $query = mysql_query($myquery);
+    
+    if ( ! $query ) {
+        echo mysql_error();
+        die;
+    }
+    
+    $data = array();
+    
+    for ($x = 0; $x < mysql_num_rows($query); $x++) {
+        $data[] = mysql_fetch_assoc($query);
+    }
+    
+    echo json_encode($data);     
+     
+    mysql_close($server);
+
 ?>
