fa7a4b7aaa07427b787138aa0ba8a9d895764a17
Matt Giguere <matthew.giguere@gmail.com>
2014-11-17 16:31:57 -0500

    added removeObservation.py
    will remove the given obsid from all tables in the DB

---
+++ removeObservation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""
+Created on 2014-11-17T16:29:17
+"""
+
+from __future__ import division, print_function
+import sys
+import argparse
+
+try:
+    import numpy as np
+except ImportError:
+    print('You need numpy installed')
+    sys.exit(1)
+
+
+__author__ = "Matt Giguere (github: @mattgiguere)"
+__maintainer__ = "Matt Giguere"
+__email__ = "matthew.giguere@yale.edu"
+__status__ = " Development NOT(Prototype or Production)"
+__version__ = '0.0.1'
+
+
+def removeObservation(arg1, arg2):
+    """PURPOSE: To remove an observation from all tables."""
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='argparse object.')
+    parser.add_argument(
+        'arg1',
+        help='This argument does something.')
+    parser.add_argument(
+        'arg2',
+        help='This argument does something else. By specifying ' +
+             'the "nargs=>" makes this argument not required.',
+             nargs='?')
+    if len(sys.argv) > 3:
+        print('use the command')
+        print('python removeObservation.py obsid')
+        sys.exit(2)
+
+    args = parser.parse_args()
+
+    removeObservation(int(args.arg1), args.arg2)
+ 
