56bafbdf03cb9d50f152220e608ef23162ea2471
Matt Giguere <matthew.giguere@gmail.com>
2014-11-17 16:51:29 -0500

    added a few tidbits
    imports the appropriate modules and sets of the SQL query

--- removeObservation.py
+++ removeObservation.py
@@ -7,11 +7,17 @@ Created on 2014-11-17T16:29:17
 from __future__ import division, print_function
 import sys
 import argparse
+import subprocess
 
 try:
     import numpy as np
 except ImportError:
     print('You need numpy installed')
+    sys.exit(1)
+try:
+    import pymysql
+except:
+    print('You need pymysql installed')
     sys.exit(1)
 
 
@@ -22,21 +28,58 @@ __status__ = " Development NOT(Prototype or Production)"
 __version__ = '0.0.1'
 
 
-def removeObservation(arg1, arg2):
+def getAeroDir():
+    cmd = 'echo $AeroFSdir'
+    #read in the AeroFSdir string and
+    adir = subprocess.check_output(cmd, shell=True)
+    #chop off the newline character at the end
+    adir = adir[0:len(adir)-1]
+    return adir
+
+
+def connectChironDB():
+    """connect to the database"""
+    #retrieve credentials:
+    adir = getAeroDir()
+    credsf = open(adir+'.credentials/SQL/csaye', 'r')
+    creds = credsf.read().split('\n')
+    conn = pymysql.connect(host=creds[0],
+                           port=int(creds[1]),
+                           user=creds[2],
+                           passwd=creds[3],
+                           db=creds[4])
+    #cur = conn.cursor()
+    return conn
+
+
+def removeObservation(obsid):
     """PURPOSE: To remove an observation from all tables."""
+
+    #the MySQL command that will retrieve the observation_id
+    #of the erroneous entry:
+    cmd = "SELECT observation_id FROM observations "
+    cmd += " WHERE obsid = " + str(obsid)
+
+    #connect to the chiron database
+    conn = connectChironDB()
+    cur = conn.cursor()
+
+    cur.execute(cmd)
+    observation_id = cur.fetchall()
+
+    #restore the list of table names from which
+    #this observation_id will be removed
+    tableFrame = pd.read_csv('tables/tableList.txt')
+    self.tableNames = tableFrame.tableName
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='argparse object.')
+        description='removeObservation.py will remove an observation' +
+                    'from all tables in the database.')
     parser.add_argument(
-        'arg1',
-        help='This argument does something.')
-    parser.add_argument(
-        'arg2',
-        help='This argument does something else. By specifying ' +
-             'the "nargs=>" makes this argument not required.',
-             nargs='?')
+        'obsid',
+        help='The unique obsid of the observation.')
     if len(sys.argv) > 3:
         print('use the command')
         print('python removeObservation.py obsid')
@@ -44,5 +87,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    removeObservation(int(args.arg1), args.arg2)
- 
+    removeObservation(args.obsid)
